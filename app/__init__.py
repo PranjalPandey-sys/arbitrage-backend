c596fb5dfc7f0ffbc8073875569671c269bb1cff
PranjalPandey-sys <420pranjalpandey@gmail.com>
2025-09-22 10:38:56 +0530

    Add files via upload

---
+++ app/__init__.py
@@ -0,0 +1,5 @@
+"""Arbitrage Detection Backend Package"""
+
+__version__ = "1.0.0"
+__author__ = "Arbitrage Detection Team"
+__description__ = "FastAPI backend for sports arbitrage detection and calculation"
